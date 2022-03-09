1cb88de031b06159de606f5e9d6e7642817729e4
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2022-03-09 22:29:00 +0600

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,12 @@ const fetchIssues = () => {
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              ${status === "Closed" ? `
+                                 <h3><s> ${description} </s></h3>
+                                `: 
+                                `
+                                  <h3> ${description} </h3>
+                                `}
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
                               <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
